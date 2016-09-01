9f09e8e40e1e6cecb0cc34b3edfa419ad9fc898c
fluke1030 <dagellapsychocut475@gmail.com>
2016-09-01 18:37:33 +0700

    Add files via upload

--- app/pages/promotion/promotion.ts
+++ app/pages/promotion/promotion.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LoginPage } from '../login/login';
 import { PromotionDetailPage } from '../promotion-detail/promotion-detail';
 
 @Component({
@@ -9,8 +10,12 @@ import { PromotionDetailPage } from '../promotion-detail/promotion-detail';
 export class PromotionPage {
   item: Array<string>;
   items: Array<{'img': string, 'shortdetail': string,'essologo': string,'otherlogo': string, 'detail': string}>;
+  login: Array<{'img': string}>;
 
   constructor(private navCtrl: NavController) {
+    this.login = [];
+    this.login.push({'img':'build/img/Login.jpg'});
+
     this.items = [];
     this.items.push({'img':'build/img/Tesco.jpg','shortdetail':'Tesco Points','essologo': 'build/img/Logo.png', 'otherlogo': 'build/img/smalltesco.png','detail': 'Conversely, 10,000 Tesco-Lotus Club Card Points can also be converted to 500 Esso Smiles Driver Rewards Points.'});
     this.items.push({'img':'build/img/AirAsia.jpg','shortdetail':'AirAsia Points','essologo': 'build/img/Logo.png', 'otherlogo': 'build/img/smallairasia.png','detail': 'Convert Esso Smiles Driver Rewards Points to mileages of Air Asia BigPoint for 500 points to redeem for air ticket.'});
@@ -21,6 +26,9 @@ export class PromotionPage {
     this.navCtrl.push(PromotionDetailPage, {
        item: item
      });
+  }
+  gotoLogin(){
+    this.navCtrl.push(LoginPage);
   }
   
 }
