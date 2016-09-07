48e87f72da71cc46ae429aef9e24841b2425b23c
PipatM <chulafromact@gmail.com>
2016-09-07 16:14:14 +0700

    update profile page

--- app/pages/profile/profile.ts
+++ app/pages/profile/profile.ts
@@ -11,9 +11,10 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'build/pages/profile/profile.html',
 })
 export class ProfilePage {
-
+    
+    profile: {'name': string, 'tel': string, 'email': string};
   constructor(private navCtrl: NavController) {
-
+    this.profile = {'name': 'Mr. Pipat Mungchitwisawakor', 'tel': '081-234-5678', 'email': 'abc@gmail.com'};
   }
 
 }
